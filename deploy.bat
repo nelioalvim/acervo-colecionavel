c4b2ae642c2441f3855d69604471cb56bcff0e55
nelioalvim <nelioalvim@gmail.com>
2026-06-29 21:20:22 -0400

    atualizacao

---
+++ deploy.bat
@@ -0,0 +1,9 @@
+@echo off  
+cd /d "C:\Users\nelio\Documents\GitHub\acervo-colecionavel"  
+git pull  
+git add .  
+git commit -m "atualizacao"  
+git push  
+echo.  
+echo PRONTO! Site atualizando em 1 minuto.  
+pause 
